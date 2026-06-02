68a894bf67217b8e88e134ae3cd314a2b3281e6c
Shoma Shimokochi <shoma0503onigiri@gmail.com>
2026-06-02 17:41:05 +0900

    Add CoordinateInput

--- src/CoordinateInput.tsx
+++ src/CoordinateInput.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './assets/CoordinateInput.css';
 import Message from './Message';
+import axisImg from "./assets/axis.png"
 
 function CoordinateInput() {
 
@@ -8,7 +9,12 @@ function CoordinateInput() {
 
     return (
         <div id="coordinate_input">
-            
+            <div id="coordinate_plane">
+                <div id="y_axis_label" className="axis_label">Y Coordinate</div>
+                <div id="x_axis_label" className="axis_label">X Coordinate</div>
+                <div id="pointer"></div>
+                <img src={axisImg} id="axis_img"></img>
+            </div>
         </div>
     );
 }
