710cd7df52b5d6f5bcdc936aa2bcd56a8429fa64
Shoma Shimokochi <shoma0503onigiri@gmail.com>
2026-06-02 23:07:29 +0900

    Add value indicator

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,20 @@ import CoordinateInput from './CoordinateInput';
 
 function App() {
 
-    const [msg, setMsg] = useState("Hello World")
-
     return (
         <div>
             <div id="logo">C3 Project</div>
             <CoordinateInput></CoordinateInput>
+            <div id="value_area">
+                <div className='value_wrapper'>
+                    <div id="x_value_label" className='value_label'>X Coordinate</div>
+                    <div id="x_value" className='value'>90%</div>
+                </div>
+                <div className='value_wrapper'>
+                    <div id="y_value_label" className='value_label'>Y Coordinate</div>
+                    <div id="y_value" className='value'>90%</div>
+                </div>
+            </div>
             {/* <Message text="Hello React"></Message> */}
         </div>
     );
